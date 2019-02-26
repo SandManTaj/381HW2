8443d070303eb85dc10dd74dfd80edd344b514b1
YIMBZ <31938596+YIMBZ@users.noreply.github.com>
2019-02-26 15:34:03 -0800

    added much needed comments

--- _381HW2P3.py
+++ _381HW2P3.py
@@ -3,25 +3,37 @@ import sys
 
 #p0=0.6;   e0=0.05;   e1=0.03
 
+#creates a random message that is either 0 or 1 based on the
+#array argument
 def message(arr):
+    #exits the program if the total probability is not 1
     if (np.sum(arr) != 1.0):
         sys.exit("Total probability is not 1!")
-    arrsum = np.cumsum(arr)
-    cp = np.append(0, arrsum)
-    k = len(arr)
-    r = np.random.random()
+    arrsum = np.cumsum(arr) #converts the array into a cumulative sum array
+    cp = np.append(0, arrsum) #appends a 0 to the front of the array
+    k = len(arr) #gets the length of the array
+    r = np.random.random() #gets a random number between 0 and 1
+    #returns a number based on the random number and the probability array
     for i in range(0, k):
         if r > cp[i] and r <= cp[i+1]:
             d = i
             return d
+    return -1
 
+#creates errors in binary array
 def error(p):
-    arr = np.copy(p)
+    arr = np.copy(p) #creates a copy of the array in order to avoid editting the original array
+    #loops through the whole array and adds errors based on the probability of error
     for i in range(0, len(arr)):
+        #if the number in the array is not binary, the program exits
+        if (arr[i] != 1 or arr[i] != 0):
+            sys.exit("Incorrect input!")
+        #potentially replaces the current 0 with 1
         if (arr[i] == 0):
             r = np.random.random()
             if (r <= 0.05):
                 arr[i] = 1
+        #potentially replaces the current 1 with 0
         if (arr[i] == 1):
             r = np.random.random()
             if (r <= 0.03):
@@ -29,14 +41,18 @@ def error(p):
     return arr
 
 def main():
-    prob = np.array([0.6, 0.4])
-    sArr = np.array([])
-    counter = 0
-    sCounter = 0
+    prob = np.array([0.6, 0.4]) #array to store the probabilty
+    sArr = np.array([]) #array to store sent messages
+    counter = 0 #counts the number of times 1 was correctly recieved
+    sCounter = 0 #counts the number of times 1 was sent
+    #creates a random array of sent messages
     for i in range(0, 100000):
         s = message(prob)
         sArr = np.append(sArr, s)
-    rArr = error(sArr)
+    rArr = error(sArr) #creates an erray of recieved messages with a
+                       #potential for error
+    #loops through the arrays and counts the number of times a 1 was 
+    #recieved and a 1 was sent
     for i in range(0, len(sArr)):
         if (rArr[i] == 1):
             counter += 1
--- _381HW2P4.py
+++ _381HW2P4.py
@@ -3,13 +3,16 @@ import sys
 
 #p0=0.6;   e0=0.05;   e1=0.03
 
+#creates a random number string based off of a given probability
 def message(p):
+    #exits if the total probability is not 1
     if (np.sum(p) != 1.0):
         sys.exit("Total probability is not 1!")
-    arrsum = np.cumsum(p)
-    cp = np.append(0, arrsum)
-    k = len(p)
-    r = np.random.random()
+    arrsum = np.cumsum(p) #creates a cumulative sum with the probability
+    cp = np.append(0, arrsum) #appends a 0 to the front of the cumulative prob array
+    k = len(p) #stores the length of the argument array
+    r = np.random.random() #creates a random number between 0 and 1
+    #returns a random string 000 or 111 based on the probability
     for i in range(0, k):
         if r > cp[i] and r <= cp[i+1]:
             d = i
@@ -17,15 +20,20 @@ def message(p):
                 return '000'
             else:
                 return '111'
+    return -1
 
+#creates errors in the sent messages based on probability
 def error(p):
-    arr = np.copy(p)
+    arr = np.copy(p) #creates a copy of the array to avoid editting the original
+    #loops through the array to create error
     for i in range(0, len(arr)):
+        #potentially changes one or more zeros to ones
         if (arr[i] == '000'):
             for j in range(len(arr[i])):
                 r = np.random.random()
                 if (r <= 0.05):
-                    arr[i] = arr[i][:j] + '1' + arr[i][j+1:]          
+                    arr[i] = arr[i][:j] + '1' + arr[i][j+1:]      
+        #potentially changes one or more ones to zeros    
         if (arr[i] == '111'):
             for j in range(len(arr[i])):
                 r = np.random.random()
@@ -33,31 +41,39 @@ def error(p):
                     arr[i] = arr[i][:j] + '0' + arr[i][j+1:]
     return arr
 
+#function which aims to 'fix' error in the recieved messages
 def errorCheck(p):
-    arr = np.copy(p)
+    arr = np.copy(p) #creates a copy of user supplied array
+    #loops through the array to fix erroneous messaged
     for i in range(0, len(arr)):
-        ones = 0
-        zero = 0
+        ones = 0 #counts the number of ones in an array
+        zero = 0 #counts the number of zeros in an array
+        #loops through a single message and determines the number of 1s and 0s
         for j in range(0, len(arr[i])):
             if(arr[i][j] == '0'):
                 zero += 1
             if(arr[i][j] == '1'):
                 ones += 1
+        #sets the message to 000 if there were more 0s
         if(zero > ones):
             arr[i] = '000'
+        #sets the message to 111 if there were more 1s
         else:
             arr[i] = '111'
     return arr
 
 def main():
-    prob = np.array([0.6, 0.4])
-    sArr = np.array([])
-    counter = 0
+    prob = np.array([0.6, 0.4]) #an array to store porbabilities
+    sArr = np.array([]) #an array to store sent messages
+    counter = 0 #an array to count the number of times a sent message id the same
+                #as the recieved one
+    #creates 100000 messages
     for i in range(0, 100000):
         s = message(prob)
         sArr = np.append(sArr, s)
-    rArr = error(sArr)
-    eArr = errorCheck(rArr)
+    rArr = error(sArr) #adds errors to the sent messages
+    eArr = errorCheck(rArr) #attempts to repair erroneous messages
+    #loops through the array and counts the number of correctly recieved messages
     for i in range(0, len(sArr)):
         if (sArr[i] == eArr[i]):
             counter += 1
